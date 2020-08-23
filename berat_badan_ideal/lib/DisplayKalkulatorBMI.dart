656bce6652e5302ab4de1d871b702bcdc4fd2fc1
agungsaptahs <68907445+agungsaptahs@users.noreply.github.com>
2020-08-23 11:56:08 +0900

    add_dart_class
    Display

---
+++ berat_badan_ideal/lib/DisplayKalkulatorBMI.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class DisplayKalkulatorBMI extends StatefulWidget {
+  @override
+  _DisplayKalkulatorBMIState createState() => _DisplayKalkulatorBMIState();
+}
+
+class _DisplayKalkulatorBMIState extends State<DisplayKalkulatorBMI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
--- berat_badan_ideal/lib/main.dart
+++ berat_badan_ideal/lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(kalkulatorBMI());
 class kalkulatorBMI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    return MaterialApp(
+      title: 'Kalkulator BMI',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: DisplayKalkulatorBMI(),
     );
   }
 }
